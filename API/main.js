0fb9e19bdb94fa2f49b1097f168c3c3e216284dd
deivinot <davidmaso2001@gmail.com>
2024-03-04 10:21:33 +0000

    intentar conectar api

---
+++ API/main.js
@@ -0,0 +1,41 @@
+
+let operacionActual = '';
+let numeros = [];
+
+function actualizarDisplay() {
+    document.getElementById('console').value = numeros.join('') + operacionActual;
+
+}
+function agregarNumero(numero) {
+    numeros.push(numero);
+    document.getElementById('console').innerText += value;
+    actualizarDisplay();
+}
+
+function realizarOperacion(operacion) {
+    operacionActual = operacion;
+    document.getElementById('console').innerText += value;
+    actualizarDisplay();
+}
+
+function limpiar() {
+    numeros = [];
+    operacionActual = '';
+    actualizarDisplay();
+}
+
+function calcularResultado() {
+    const expresion = numeros.join('') + operacionActual;
+    // Realizar solicitud a la API para calcular el resultado
+    fetch(`http://localhost:3000/calcular?expresion=${encodeURIComponent(expresion)}`)
+        .then(response => response.json())
+        .then(data => {
+            // Mostrar el resultado en el display
+            document.getElementById('display').value = data.resultado;
+        })
+        .catch(error => {
+            console.error('Error al realizar la solicitud:', error);
+            document.getElementById('display').value = 'Error';
+        });
+}
+
