269fcbde39ad7395afd0302e3575d9b1c89069ce
deivinot <davidmaso2001@gmail.com>
2024-03-13 19:06:02 +0000

    crear funcion verificar cadena

--- API/main.js
+++ API/main.js
@@ -1,6 +1,9 @@
-
 let operacionActual = '';
 let numeros = [];
+let numbers;
+let operations;
+let result;
+
 function chainValidation(chain){
     const error= new Error("Syntax error");
     const fail  = /[^0-9]{2}/;   //cadena para que no hayan dos símbolos juntos
@@ -21,21 +24,22 @@ function chainValidation(chain){
 
 }
 function splitChain(chain){
+    const pattern = /[^0-9]/; // Coincide con cualquier caracter que no sea un número
+    const pattern2 = /[0-9]/; // Coincide con los caracteres que sean números
+
+    numbers = chain.split(pattern);
+    operations = chain.split(pattern2);
+    operations = operations.filter(str => str.length > 0);
+
 
 }
 function limpiar(){
     document.getElementById('console').innerText = "";
-
+    console.clear();
 }
 
-function sumar() {
-    const a = document.getElementById("a").value;
-    const b = document.getElementById("b").value;
-    console.log(a);
-    console.log(b);
-
-
-    fetch(`http://127.0.0.1:8000/suma_basica/${a}+${b}`)
+function operacion(a, b, op) {
+    fetch(`http://127.0.0.1:8000/${op}/${a}+${b}`)
         .then(function (response) {
             if (!response.ok) {
                 throw new Error(`Error de red - Código de estado: ${response.status}`);
@@ -44,13 +48,47 @@ function sumar() {
         })
         .then(function (data) {
             console.log(data);
-            document.getElementById("resultado").innerText = `Resultado: ${data}`;
-
+            //document.getElementById("resultado").innerText = `Resultado: ${data}`;
+            result = data;
         })
         .catch(error => {
             console.error('Error al llamar a la API:', error);
         });
 
 
+}
+
+function obtenerOperacion(simbolo) {
+    switch (simbolo) {
+        case "+":
+            return "suma_basica";
+        case "-":
+            return "resta_basica";
+        case "*":
+            return "multiplicacion_basica";
+        case "/":
+            return "division_basica";
+        default:
+            return "Operación no válida";
+    }
+}
+function principal (cadena){
+    chainValidation(cadena);
+    splitChain(cadena)
+    let a;
+    let b;
+    let op;
+
+    while(operations.length !== 0){
+        a = numbers.shift();
+        b = numbers.shift();
+        op = operations.shift();
+        op = obtenerOperacion(op);
+        console.log(a);
+        console.log(b);
+        console.log(op);
+        operacion(a, b, op);
+        numbers.unshift("coger resultado");
+    }
 }
 
